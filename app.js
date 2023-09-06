b628feb5951c531b32362a1cc5ee221bf31031c2
Santiago <santiagoruth1704@gmail.com>
2023-09-06 14:09:01 -0300

    myfirst commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+const PORT=process.env.PORT || 3001;
+
+app.use(express.static("public"))
+
+app.get("/",function (req, res) {
+    
+    res.sendFile(path.join(__dirname,"views","index.html"))
+    
+})
+
+
+app.get("/views/register",function (req, res) {
+    
+    res.sendFile(path.join(__dirname,"views","register.html"))
+    
+})
+
+app.get("/views/login",function (req, res) {
+    
+    res.sendFile(path.join(__dirname,"views","login.html"))
+    
+})
+
+
+// app.get("/prueba", function(req, res){
+//     res.send("soy prueba")
+    
+// })
+
+app.listen (PORT,() => console.log(`Servidor escuchando en puerto ${PORT}`))
